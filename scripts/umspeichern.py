a5ef7b0741ec35f2978df5a49d4feab1e093bf37
mleipert <martin.leipert@th-deg.de>
2023-03-05 17:29:36 +0100

    ignore tags in file

--- scripts/umspeichern.py
+++ scripts/umspeichern.py
@@ -16,6 +16,28 @@ with open(path, "rb") as openfile:
 raw_text = raw_text.decode("utf-8")
 json_dict = json.loads(raw_text)
 
+
+# Remove lists from tags
+
+new_annotations = []
+
+for item in json_dict["annotations"]:
+
+    new_item = {}
+
+    for ikey, ivalue in item.items():
+
+        # ignore the tags => causes errors
+        if ikey == "tags":
+            continue
+
+        new_item[ikey] = ivalue
+
+    new_annotations.append(new_item)
+
+
+json_dict["annotations"] = new_annotations
+
 path_2 = os.path.join(path.parent, f"{path.stem}_Copy.json")
 
 with open(path_2, "w") as outfile:
